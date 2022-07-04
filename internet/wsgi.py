341eef44177fd5958553d24833bbd4802d7a9e64
freddylimachi <freddylo_@hotmail.com>
2022-07-04 17:57:37 -0500

    starting app 5

--- internet/wsgi.py
+++ internet/wsgi.py
@@ -1,11 +1,4 @@
-"""
-WSGI config for internet project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -13,4 +6,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internet.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
