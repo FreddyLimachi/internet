eb35d9c0626e6e0a99a8d750710582fe4639a2c7
freddylimachi <freddylo_@hotmail.com>
2022-07-04 17:45:34 -0500

    startip app 4

--- internet/settings.py
+++ internet/settings.py
@@ -166,4 +166,5 @@ JAZZMIN_SETTINGS = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR, 'static')
